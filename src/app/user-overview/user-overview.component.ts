743b44deb0a6d6f1e5c7453c80e29eb6172ee037
Khanh Huynh <khanh.huynh.sync@gmail.com>
2024-06-23 15:53:47 +0700

    feat: do makeup

--- src/app/user-overview/user-overview.component.ts
+++ src/app/user-overview/user-overview.component.ts
@@ -1,9 +1,12 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {UserDetailComponent} from "../user-detail/user-detail.component";
 
 @Component({
   selector: 'app-user-overview',
   standalone: true,
-  imports: [],
+  imports: [
+    UserDetailComponent
+  ],
   templateUrl: './user-overview.component.html',
   styleUrl: './user-overview.component.css'
 })
